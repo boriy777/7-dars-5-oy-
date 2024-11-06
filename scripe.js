356ff74582fd1e076d0a3429ba4ebd5090eeed56
boriy777 <abdulboriy822@gmail.com>
2024-11-06 15:36:45 +0500

    Add files via upload

---
+++ scripe.js
@@ -0,0 +1,11 @@
+let array = [2,54,21,56,]
+let juft = []
+for (let i = 0; i < array.length; i++) {
+   if (array[i]% 2 == 0) {
+  
+juft.push(array[i])
+   
+   }
+}
+console.log(array);
+console.log(juft);
